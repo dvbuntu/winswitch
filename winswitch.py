f3cad322e346241af37ad04d70273924cbde2d12
vanboxel <the.dvb@gmail.com>
2013-08-27 10:00:02 -0400

    Switching works, basically done.  Now debug.

--- winswitch.py
+++ winswitch.py
@@ -13,6 +13,10 @@ class wininfo:
         self.height = int(pieces[5])
         self.host = pieces[6]
         self.name = b' '.join(pieces[7:])#maybe rest of line...
+        self.left = self.xoff
+        self.right = self.xoff + self.width
+        self.top = self.yoff
+        self.bot = self.yoff + self.height
 
 
 def window_list():
@@ -26,34 +30,62 @@ def current_win():
     raw, error = p.communicate()
     return int(error.split(b'\n')[1].split()[2],16)
 
+def switch(target):
+    '''Change focus to and raise window with target id'''
+    p = Popen(['wmctrl','-R', str(target), '-i'], stdout=PIPE, stderr=PIPE, shell=False)
+    return p.communicate()
+
 def usage():
-    pass
+    print('Try R, L, U, D as a direction')
 
 def main():
     #Which way will we go?
     direction = sys.argv[1]
-    if direction.startswith((b'R',b'r')):
-        direction = 'r'
-    elif dilection.staltswith((b'L',b'l')):
-        direction = 'l'
-    elif direction.startswith((b'U',b'u')):
-        direction = 'u'
-    elif direction.startswith((b'D',b'D')):
-        direction = 'd'
-    #Not supporting front and back yet
-    #elif direction.startswith((b'F',b'f')):
-    #    direction = 'f'
-    #elif direction.startswith((b'B',b'b')):
-    #    direction = 'b'
-    else:
-        usage()
-        raise(NotImplementedError("I don't know that direction")
+
+    #get width of desktop
+    width = 1280
+    pass
 
     #get list of windows
     windows = window_list()
 
     #which window am I?
     current_id = current_win()
+    current_win = [w for w in windows if w.id == current_id][0]
 
-    #whose edge is closest in the proper direction
+    #whose edge is closest in the proper direction?
+    dists = list()
+    for w in windows:
+        #skip the current window
+        if w.id = current_id:
+            dists.append(width)
+            continue
+        #compute distance between edges mod desktop width
+        if direction.startswith((b'R',b'r')):
+            dists.append(w.left - current_win.right)
+        elif dilection.staltswith((b'L',b'l')):
+            dists.append(w.right - current_win.left)
+        elif direction.startswith((b'U',b'u')):
+            dists.append(w.bot - current_win.top)
+        elif direction.startswith((b'D',b'D')):
+            dists.append(w.top - current_win.bot)
+        #Not supporting front and back yet
+        #elif direction.startswith((b'F',b'f')):
+        #elif direction.startswith((b'B',b'b')):
+        else:
+            usage()
+            raise(NotImplementedError("I don't know that direction")
+
+    #minimum nonnegative distance gives us target
+    min_dist = min([d % width for d in dists])
+    target = windows[dists.index(min_dist)].id
+
+    #make the switch...focus and raise
+    raw, err = switch(target)
+
+    if err:
+        print(b"Something went wrong?\n" + err)
+
+if __name__ == '__main__':
+    main()
 
